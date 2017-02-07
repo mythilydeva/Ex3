4fa0743176169cedf0d2a32bf5ba6388656cd8b0
ITLAB <ITLAB@IT212>
2017-02-07 15:58:10 +0530

    commit7

--- src/com/example/graphics/MainActivity.java
+++ src/com/example/graphics/MainActivity.java
@@ -2,23 +2,17 @@ package com.example.graphics;
 
 import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.os.Bundle;  
+import android.widget.TextView;  
 
 
 public class MainActivity extends ActionBarActivity {
 	Button rect,sq,cir,str;
 	TextView text;
-	EditText ed1,ed2,ed3,ed4;
+	EditText ed1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,17 +24,17 @@ public class MainActivity extends ActionBarActivity {
         str=(Button) findViewById(R.id.str);
         text=(TextView) findViewById(R.id.text);
         ed1=(EditText) findViewById(R.id.ed1);
-        ed2=(EditText) findViewById(R.id.ed2);
-        ed3=(EditText) findViewById(R.id.ed3);
-        ed4=(EditText) findViewById(R.id.ed4);
+        
         
         rect.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
 				String data="rectangle";
+				String val=ed1.getText().toString();
 				Intent i=new Intent(MainActivity.this,SecondActivity.class);
 				i.putExtra("value", data);
+				i.putExtra("dim", val);
 				startActivity(i);
 			}
 		});
@@ -48,9 +42,11 @@ public class MainActivity extends ActionBarActivity {
 			
 			@Override
 			public void onClick(View arg0) {
+				String val=ed1.getText().toString();
 				String data="square";
 				Intent i=new Intent(MainActivity.this,SecondActivity.class);
 				i.putExtra("value", data);
+				i.putExtra("dim", val);
 				startActivity(i);
 			}
 		});
@@ -58,9 +54,11 @@ public class MainActivity extends ActionBarActivity {
 		
 		@Override
 		public void onClick(View arg0) {
+			String val=ed1.getText().toString();
 			String data="circle";
 			Intent i=new Intent(MainActivity.this,SecondActivity.class);
 			i.putExtra("value", data);
+			i.putExtra("dim", val);
 			startActivity(i);
 		}
 	});
@@ -68,9 +66,11 @@ public class MainActivity extends ActionBarActivity {
 		
 		@Override
 		public void onClick(View arg0) {
+			String val=ed1.getText().toString();
 			String data="string";
 			Intent i=new Intent(MainActivity.this,SecondActivity.class);
 			i.putExtra("value", data);
+			i.putExtra("dim", val);
 			startActivity(i);
 		}
 	});
--- src/com/example/graphics/SecondActivity.java
+++ src/com/example/graphics/SecondActivity.java
@@ -3,20 +3,18 @@ package com.example.graphics;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.app.Activity;
 import android.content.Intent;
-import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 
 public class SecondActivity extends Activity
 {
-	String data;
+	String data,d;
+	int dim,dim1;
 	 GraphicsView graphicsview;  
 	     
 	protected void onCreate(Bundle  savedInstanceState)
@@ -40,6 +38,9 @@ public class SecondActivity extends Activity
 					super.onDraw(canvas);
 					Intent i=getIntent();
 					data= i.getStringExtra("value");
+					d=i.getStringExtra("dimension");
+					dim=Integer.parseInt(d);
+					dim1=dim*2;
 					// custom drawing code here
 					Paint paint = new Paint();
 					paint.setStyle(Paint.Style.FILL);
@@ -47,35 +48,30 @@ public class SecondActivity extends Activity
 					// make the entire canvas white
 					paint.setColor(Color.CYAN);
 					canvas.drawPaint(paint);
-					canvas.drawRect(10,10,10,10, paint);
 			
 					if(data.equals("rectangle")){
 					paint.setAntiAlias(false);
 					paint.setColor(Color.MAGENTA);
-					canvas.drawRect(100, 100,100,100, paint);
+					canvas.drawRect(10, 10,dim,dim1, paint);
 					}
 		 
 				
 					if(data.equals("circle")){
 					paint.setAntiAlias(true);
 					paint.setColor(Color.GRAY);
-					canvas.drawCircle(60, 20, 15, paint);
+					canvas.drawCircle(60, 20, dim, paint);
 					}
 		 
 					if(data.equals("square")){
 					paint.setAntiAlias(false);
 					paint.setColor(Color.RED);
-					canvas.drawRect(100, 5, 200, 30, paint);
-					}
-		 
+					canvas.drawRect(10, 5, dim, dim, paint);
+					}		 
 					canvas.rotate(-40);
-					if(data.equals("string")){
-		 
+					if(data.equals("string")){		 
 					paint.setStyle(Paint.Style.FILL);
 					canvas.drawText("Graphics Rotation", 45, 185, paint);
 					}
-		 
-					// undo the rotate
 					canvas.restore();
 				}
 			}
