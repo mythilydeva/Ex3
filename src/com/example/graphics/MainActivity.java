33a5904ecd6aa4269e83fbc9393f1ffe90478143
ITLAB <ITLAB@IT212>
2017-02-07 13:28:05 +0530

    project created

---
+++ src/com/example/graphics/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.graphics;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+	Button rect,sq,cir,str;
+	TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    
+}
